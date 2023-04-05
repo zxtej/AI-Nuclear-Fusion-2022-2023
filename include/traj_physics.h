c0ad8aaa2ec584cdd89100e923da27e151bf3cb1
pcklee123 <pcklee123@gmail.com>
2023-04-06 06:13:15 +0800

    updates

--- include/traj_physics.h
+++ include/traj_physics.h
@@ -1,3 +1,39 @@
+// #define FileIn //whether to load from input file (unused)
+#define cldevice 1 //which GPU to use
+#define RamDisk // whether to use RamDisk
+#define maxcells 32 //maximum cel number to outputy
+
+// The maximum expected E and B fields. If fields go beyond this, the the time step, cell size etc will be wrong. Should adjust and recalculate.
+//  maximum expected magnetic field
+constexpr float Bmax0 = 1;
+constexpr float Emax0 = 1e12;
+
+constexpr float target_part = 1e15;
+
+#define Temp_e 1e8 // in Kelvin
+#define Temp_d 1e8 // in Kelvin
+#define Hist_n 256
+#define Hist_max Temp_e / 11600 * 20 // in eV Kelvin to eV is divide by 11600
+
+constexpr float a0 = 2e-3; // typical dimensions of a cell
+
+// technical parameters
+constexpr int n_space = 128;                              // must be 2 to power of n
+constexpr int n_partd = n_space * n_space * n_space * 1; // must be 2 to power of n
+constexpr int n_parte = n_partd;
+constexpr float r_part_spart = target_part / n_partd; // 1e12 / n_partd; // ratio of particles per tracked "super" particle
+// ie. the field of N particles will be multiplied by (1e12/N), as if there were 1e12 particles
+
+constexpr unsigned int ncoeff = 8;
+
+constexpr int n_output_part = (n_partd > 8192) ? 8192 : n_partd; // maximum number of particles to output to file
+// const int nprtd=floor(n_partd/n_output_part);
+
+constexpr int ndatapoints = 300; // total number of time steps to calculate
+constexpr int nc = 10;           // number of times to calculate E and B between printouts
+constexpr int md_me = 60;        // ratio of electron speed/deuteron speed at the same KE. Used to calculate electron motion more often than deuteron motion
+
+
 #define trilinon_
 #define Uon_  // whether to calculate the electric (V) potential and potential energy (U). Needs Eon to be enabled.
 #define Eon_  // whether to calculate the electric (E) field
@@ -9,40 +45,11 @@
 // #define printV //print out V
 #define printB // print out B field
 #define printE // print out E field
-// #define FileIn //whether to load from input file (unused)
-// #define RamDisk // whether to use RamDisk
-#define maxcells 32
-#define cldevice 0
 
-#define Temp_e 1e8 // in Kelvin
-#define Temp_d 1e8 // in Kelvin
-#define Hist_n 256
-#define Hist_max Temp_e / 11600 * 20 // in eV Kelvin to eV is divide by 11600
 
-constexpr float a0 = 200e-3; // typical dimensions of a cell
-// technical parameters
-constexpr int n_space = 64;                              // must be 2 to power of n
-constexpr int n_partd = n_space * n_space * n_space * 1; // must be 2 to power of n
-constexpr int n_parte = n_partd;
-constexpr unsigned int ncoeff = 8;
-
-constexpr int n_output_part = (n_partd > 8192) ? 8192 : n_partd; // maximum number of particles to output to file
-// const int nprtd=floor(n_partd/n_output_part);
-
-constexpr int ndatapoints = 300; // total number of time steps to calculate
-constexpr int nc = 10;           // number of times to calculate E and B between printouts
-constexpr int md_me = 60;        // ratio of electron speed/deuteron speed at the same KE. Used to calculate electron motion more often than deuteron motion
 
 
 constexpr int nthreads = 8;    // match with your CPU
-// The maximum expected E and B fields. If fields go beyond this, the the time step, cell size etc will be wrong. Should adjust and recalculate.
-//  maximum expected magnetic field
-constexpr float Bmax0 = 1;
-constexpr float Emax0 = 1e9;
-
-constexpr float target_part = 1e15;
-constexpr float r_part_spart = target_part / n_partd; // 1e12 / n_partd; // ratio of particles per tracked "super" particle
-// ie. the field of N particles will be multiplied by (1e12/N), as if there were 1e12 particles
 
 constexpr int n_space_divx = n_space;
 constexpr int n_space_divy = n_space;
@@ -64,7 +71,5 @@ constexpr float pi = 3.1415926536;
 constexpr float u0 = 4e-7 * pi;
 
 constexpr int ncalc0[2] = {md_me, 1};
-
-
 constexpr int qs[2] = {-1, 1}; // Sign of charge
 constexpr    int mp[2] = {1, 1835 * 2};
